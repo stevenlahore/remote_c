992911acfd2f99b89aafba182e2e723bfa2e10f3
steven <stevenlahore@gmail.com>
2024-12-25 23:14:07 +0800

    4commit by vscode!

--- source/l1/c001.c
+++ source/l1/c001.c
@@ -1,12 +1,30 @@
 //this is c programing  this line for test !
 #include <stdio.h>
 #include <stdlib.h>
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+#if 0
+
+test tset
 
 
-
+#endif
 
 int main()
 { 
+	char a = 254;
+	printf("the number is %d ",a);
 	printf("Hello World\n");
 	//return 0;
 	exit(0);
